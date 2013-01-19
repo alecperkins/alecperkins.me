3cc0d42829f9df8ab2e24b580e132a1e9adfc56a
Alec Perkins <alecperkins@gmail.com>
2013-01-19 01:35:07 -0500

    Caching

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ from flask import Flask, jsonify, make_response, render_template
 
 from fetchers import fetchInstagram, fetchTwitter
 
+try:
+    import pylibmc
+    mc = pylibmc.Client(
+        servers=[os.environ.get('MEMCACHE_SERVERS')],
+        username=os.environ.get('MEMCACHE_USERNAME'),
+        password=os.environ.get('MEMCACHE_PASSWORD'),
+        binary=True
+    )
+
+# for dev
+except:
+    class DummyCache(object):
+        def get(*args, **kwargs):
+            return None
+        def set(*args, **kwargs):
+            return None
+    mc = DummyCache()
+
+
+CACHE_DURATION = 10 * 60 # seconds
+
+
 app = Flask(__name__,
     template_folder = 'ui',
     static_folder   = 'ui',
@@ -15,7 +37,6 @@ def jsonResponse(data):
     response.headers['Content-Type'] = 'application/json'
     return response
 
-DATA = {}
 
 @app.route('/')
 def hello():
@@ -23,17 +44,19 @@ def hello():
 
 @app.route('/data/instagram')
 def instagramData():
-    # TODO: Cache data
-    if not 'instagram' in DATA:
-        DATA['instagram'] = fetchInstagram()
-    return jsonResponse(DATA['instagram'])
+    data = mc.get('instagram')
+    if not data:
+        data = fetchInstagram()
+        mc.set('instagram', data, time=CACHE_DURATION)
+    return jsonResponse(data)
 
 @app.route('/data/twitter')
 def twitterData():
-    # TODO: Cache data
-    if not 'twitter' in DATA:
-        DATA['twitter'] = fetchTwitter()
-    return jsonResponse(DATA['twitter'])
+    data = mc.get('twitter')
+    if not data:
+        data = fetchTwitter()
+        mc.set('twitter', data, time=CACHE_DURATION)
+    return jsonResponse(data)
 
 
 if __name__ == '__main__':
